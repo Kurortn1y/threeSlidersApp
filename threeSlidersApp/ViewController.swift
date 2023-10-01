756b3040f73896043c2d8c62b0d1116984d28c1e
RomaRoma <lastmandanc1ng@icloud.com>
2023-10-01 16:55:37 +0200

    Initial Commit

---
+++ threeSlidersApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  threeSlidersApp
+//
+//  Created by Roman on 01.10.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
